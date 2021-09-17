99b50e317ed380a2b33b60af4f11b752d93d7129
san994 <75426313+san994@users.noreply.github.com>
2021-09-16 22:41:05 -0700

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text,} from 'react-native';
+
+export default class DailyPicScreen extends Component{
+
+  
+    render(){
+        return(
+            <Text>Daily Pic</Text>
+        )
+    }
+}
